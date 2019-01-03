d69db7b0b8a76834cd88a8586f8cc77990152380
jkwon <jkwon@jkwon-lt.ku.kettering.edu>
2019-01-02 20:46:39 -0500

    stack overflow test

--- test2/main.c
+++ test2/main.c
@@ -2,9 +2,18 @@
 
 /**
  * main.c
+ *
+ *
+ *  stack overflow
  */
 int main(void)
 {
+    int big[5000]; // a big array
+    int c = 4;
+    big[499] = 2;
+
+    big[4999] = c; // change the last element --> access to the outside of the stack.
+
     // another test for the 2nd projet
 	return 0;
 }
