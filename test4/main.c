2eab3cfe4bd24f2080394bb78a3aa59fade6018b
jkwon <jkwon@jkwon-lt.ku.kettering.edu>
2019-01-03 00:01:36 -0500

    add test more.

--- test4/main.c
+++ test4/main.c
@@ -13,6 +13,12 @@ typedef struct {
 
 //Point p1 = { 0x12, 0x3456};
 Point p1;
+int32_t aaa = -1;
+uint16_t bbb = 0x1234;
+
+int16_t arr[4] = {
+    1, 2, 3, 4
+};
 
 int main(void)
 {
@@ -21,6 +27,13 @@ int main(void)
 
     p = &p1;
     p->x = 0xFF;
+
+    int32_t ccc;
+    ccc = aaa;
+
+    int16_t bb;
+    bb = arr[2];
+
 
     return 0;
 }
