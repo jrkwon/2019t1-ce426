4f0312cc63b9633ef9b54b4821082f4786bafbcb
jkwon <jkwon@jkwon-lt.ku.kettering.edu>
2019-02-07 11:33:15 -0500

    changes for switches

--- Lectures/Lecture5/main.asm
+++ Lectures/Lecture5/main.asm
@@ -1,11 +1,20 @@
     .global main    ; makes “main” accessible from outside this file.
+
+    .data
+
+var1    .word 0x22000008
+
     .text   ; set the following section to flash (RO). used for code.
+
+
 ; Add constants for the program
 SYSCTL_RCGCGPIO_R   .field  0x400FE608
+
 main                ; must be “main.” the Startup code of CCS
                     ; uses this name.
 ; Add your code here
 ; ------------------
+
     LDR  R0, SYSCTL_RCGCGPIO_R
     LDR  R1,[R0]      ; read previous value
     ORR  R1,R1,#0x20  ; set bit 5
